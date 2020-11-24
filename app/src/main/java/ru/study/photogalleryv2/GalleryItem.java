f4c1cb5bc39113366c60b9dad9201439d65b2fd0
Crush <art60@rambler.ru>
2020-11-24 10:17:39 +0300

    Add model

---
+++ app/src/main/java/ru/study/photogalleryv2/GalleryItem.java
@@ -0,0 +1,39 @@
+package ru.study.photogalleryv2;
+
+import androidx.annotation.NonNull;
+
+public class GalleryItem {
+    private String id;
+    private String caption;
+    private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return caption;
+    }
+}
