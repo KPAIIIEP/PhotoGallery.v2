acf0b6fbd2d1c8164217245196b2f0b3dd8b4501
Crush <art60@rambler.ru>
2020-11-25 21:25:44 +0300

    Add recyclerview, request, LiveData

--- app/src/main/java/ru/study/photogalleryv2/FlickrFetch.java
+++ app/src/main/java/ru/study/photogalleryv2/FlickrFetch.java
@@ -1,12 +1,26 @@
 package ru.study.photogalleryv2;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class FlickrFetch {
     private static FlickrFetch instance;
+    private final Retrofit retrofit;
     private static final String BASE_URL = "https://api.flickr.com/services/rest/";
-    private Retrofit retrofit;
+    private static final String queryParameters = "?method=flickr.photos.getRecent"
+            + "&api_key=" + Settings.API_KEY
+            + "&format=json"
+            + "&nojsoncallback=1"
+            + "&extras=url_s";
+    private final MutableLiveData<List<GalleryItem>> liveData = new MutableLiveData<>();
 
     private FlickrFetch() {
         retrofit = new Retrofit.Builder()
@@ -22,7 +36,30 @@ public class FlickrFetch {
         return instance;
     }
 
+    public LiveData<List<GalleryItem>> getLiveData() {
+        return liveData;
+    }
+
     public FlickrAPI getAPI() {
         return retrofit.create(FlickrAPI.class);
+    }
+
+    public void requestGalleryItems() {
+        this.getAPI().getPhotos(queryParameters)
+                .enqueue(new Callback<JSONRootWrapper>() {
+                    @Override
+                    public void onResponse(Call<JSONRootWrapper> call,
+                                           Response<JSONRootWrapper> response) {
+                        List<GalleryItem> galleryItems  = response.body()
+                                .getGalleryItemWrapper()
+                                .getGalleryItems();
+                        liveData.setValue(galleryItems);
+                    }
+
+                    @Override
+                    public void onFailure(Call<JSONRootWrapper> call, Throwable t) {
+                        t.printStackTrace();
+                    }
+                });
     }
 }
--- app/src/main/java/ru/study/photogalleryv2/MainFragment.java
+++ app/src/main/java/ru/study/photogalleryv2/MainFragment.java
@@ -4,12 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainFragment extends Fragment {
     private static final String TAG = MainFragment.class.getSimpleName();
+    private RecyclerView recyclerView;
+    private PhotoAdapter photoAdapter;
 
     public static Fragment newInstance() {
         return new MainFragment();
@@ -23,6 +33,67 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        recyclerView = getActivity().findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        photoAdapter = new PhotoAdapter();
+        recyclerView.setAdapter(photoAdapter);
+        FlickrFetch instance = FlickrFetch.getInstance();
+        instance.requestGalleryItems();
+        instance.getLiveData().observe(this, new Observer<List<GalleryItem>>() {
+            @Override
+            public void onChanged(List<GalleryItem> galleryItems) {
+                photoAdapter.setPhotos(galleryItems);
+            }
+        });
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+        private GalleryItem galleryItem;
+        private TextView textView;
+
+        public PhotoHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = (TextView) itemView;
+        }
+
+        public void bindGalleryItem(GalleryItem galleryItem) {
+            this.galleryItem = galleryItem;
+            textView.setText("id: " + galleryItem.getId()
+                    + "\ncaption: " + galleryItem.getCaption()
+                    +  "\nurl: " + galleryItem.getUrl()
+                    + "\n"
+            );
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder>{
+        private List<GalleryItem> galleryItems = new ArrayList<>();
+
+        @NonNull
+        @Override
+        public PhotoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater
+                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new PhotoHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull PhotoHolder holder, int position) {
+            GalleryItem galleryItem = galleryItems.get(position);
+            holder.bindGalleryItem(galleryItem);
+        }
+
+        @Override
+        public int getItemCount() {
+            return galleryItems.size();
+        }
+
+        public void setPhotos(List<GalleryItem> items) {
+            galleryItems.clear();
+            galleryItems.addAll(items);
+            notifyDataSetChanged();
+        }
     }
 }
