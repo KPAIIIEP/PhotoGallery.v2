6d03babc0323c6fb7069480df9db46b4988848c1
Crush <art60@rambler.ru>
2020-11-30 10:21:34 +0300

    Fix fragment

--- app/src/main/java/ru/study/photogalleryv2/MainFragment.java
+++ app/src/main/java/ru/study/photogalleryv2/MainFragment.java
@@ -1,14 +1,21 @@
 package ru.study.photogalleryv2;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.paging.PagedList;
+import androidx.paging.PagedListAdapter;
+import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -20,7 +27,10 @@ import ru.study.photogalleryv2.databinding.ItemViewBinding;
 
 public class MainFragment extends Fragment {
     private static final String TAG = MainFragment.class.getSimpleName();
-    private PhotoAdapter photoAdapter;
+    private int firstVisibleItemPosition = 0;
+    private RecyclerView recyclerView;
+    private ActivityFragmentBinding binding;
+    //private PhotoAdapter adapter;
 
     public static Fragment newInstance() {
         return new MainFragment();
@@ -29,25 +39,58 @@ public class MainFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FlickrFetch instance = FlickrFetch.getInstance();
-        instance.requestGalleryItems();
-        instance.getLiveData().observe(this, new Observer<List<GalleryItem>>() {
+        setRetainInstance(true);
+
+        if (savedInstanceState != null) {
+            firstVisibleItemPosition = savedInstanceState.getInt("firstVisibleItemPosition");
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        recyclerView = getActivity().findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
-            public void onChanged(List<GalleryItem> galleryItems) {
-                photoAdapter.setPhotos(galleryItems);
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
+                //Log.d(TAG, String.valueOf(firstVisibleItemPosition));
             }
         });
+        GalleryItemViewModel viewModel =
+                new ViewModelProvider(requireActivity()).get(GalleryItemViewModel.class);
+        final PhotoAdapter adapter = new PhotoAdapter(new DiffUtil.ItemCallback<GalleryItem>() {
+            @Override
+            public boolean areItemsTheSame(@NonNull GalleryItem oldItem, @NonNull GalleryItem newItem) {
+                return oldItem.getId().equals(newItem.getId());
+            }
+
+            @SuppressLint("DiffUtilEquals")
+            @Override
+            public boolean areContentsTheSame(@NonNull GalleryItem oldItem, @NonNull GalleryItem newItem) {
+                return oldItem == newItem;
+            }
+        });
+        viewModel.pagedListLiveData.observe(requireActivity(), new Observer<PagedList<GalleryItem>>() {
+            @Override
+            public void onChanged(PagedList<GalleryItem> galleryItems) {
+                adapter.submitList(galleryItems);
+            }
+        });
+        recyclerView.setAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        ActivityFragmentBinding binding = DataBindingUtil
-                .inflate(inflater, R.layout.activity_fragment, container, false);
-        binding.recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        photoAdapter = new PhotoAdapter();
-        binding.recyclerView.setAdapter(photoAdapter);
-        return binding.getRoot();
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d(TAG, String.valueOf(firstVisibleItemPosition));
+        outState.putInt("firstVisibleItemPosition", firstVisibleItemPosition);
+        super.onSaveInstanceState(outState);
     }
 
     private class PhotoHolder extends RecyclerView.ViewHolder {
@@ -56,17 +99,18 @@ public class MainFragment extends Fragment {
         public PhotoHolder(ItemViewBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
-            binding.setViewModel(new GalleryItemViewModel());
         }
 
         public void bindGalleryItem(GalleryItem galleryItem) {
-            binding.getViewModel().setGalleryItem(galleryItem);
+            binding.setGalleryItem(galleryItem);
             binding.executePendingBindings();
         }
     }
 
-    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder>{
-        private List<GalleryItem> galleryItems = new ArrayList<>();
+    private class PhotoAdapter extends PagedListAdapter<GalleryItem, PhotoHolder> {
+        protected PhotoAdapter(@NonNull DiffUtil.ItemCallback<GalleryItem> diffCallback) {
+            super(diffCallback);
+        }
 
         @NonNull
         @Override
@@ -79,19 +123,7 @@ public class MainFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(@NonNull PhotoHolder holder, int position) {
-            GalleryItem galleryItem = galleryItems.get(position);
-            holder.bindGalleryItem(galleryItem);
-        }
-
-        @Override
-        public int getItemCount() {
-            return galleryItems.size();
-        }
-
-        public void setPhotos(List<GalleryItem> items) {
-            galleryItems.clear();
-            galleryItems.addAll(items);
-            notifyDataSetChanged();
+            holder.bindGalleryItem(getItem(position));
         }
     }
 }
